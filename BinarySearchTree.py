608d1b0a180377821cd345c06cd46bdfb4f34ecb
Himanshu Singh <singhhimanshu004@gmail.com>
2023-08-01 09:22:44 +0530

    Added Binary search tree implementation

---
+++ BinarySearchTree.py
@@ -0,0 +1,95 @@
+class Node:
+    def __init__(self, data, parent = None):
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        self.parent = parent
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data)
+
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        # We have to go to the left subtree
+        if data < node.data:
+            # left node exists: so we recursively call again the insert node function
+            if node.left_node:
+                self.insert_node(data, node.left_node)
+            # there is no left node: so, we create a left node with the incoming data and parent node
+            else:
+                node.left_node = Node(data, node)
+        # We have to go to the right subtree
+        else:
+            # right node exists, so, we recursively call again the insert node function
+            if node.right_node:
+                self.insert_node(data, node.right_node)
+            # there is no right node: so, we create the right node with the incoming data and Node
+            else:
+                node.right_node = Node(data, node)
+
+    def get_min(self):
+        if not self.root:
+            return None
+        else:
+            return self.get_min_value(self.root)
+
+    def get_min_value(self, node):
+        if node.left_node:
+            return self.get_min_value(node.left_node)
+        else:
+            return node.data
+
+    def get_max(self):
+        if not self.root:
+            return None
+        else:
+            return self.get_max_value(self.root)
+
+    def get_max_value(self, node):
+        if node.right_node:
+            return self.get_max_value(node.right_node)
+        else:
+            return node.data
+
+    def traverse(self):
+        if self.root:
+            return self.traverse_in_order(self.root)
+
+        else:
+            return None
+
+    # it has O(N) linear running time complexity
+    def traverse_in_order(self, node):
+        if node.left_node:
+            self.traverse_in_order(node.left_node)
+
+        print(node.data, end=' ')
+
+        if node.right_node:
+            self.traverse_in_order(node.right_node)
+
+
+if __name__ == '__main__':
+    bst = BST()
+    bst.insert(35)
+    bst.insert(27)
+    bst.insert(76)
+    bst.insert(56)
+    bst.insert(12)
+    bst.insert(32)
+    bst.insert(97)
+
+    print("Inorder traversal: (sorted order)")
+    bst.traverse()
+    print('\n')
+    print(f'Min value in BST: {bst.get_min()}')
+
+    print(f'Maxvalue in BST: {bst.get_max()}')
